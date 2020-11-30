ee4c394ed7d3987e26d47d31a24c2f147079e721
“Ranjith” <Ranjithroy704@gmail.com>
2020-12-01 00:21:55 +0530

    Add : Phone Number Validation Added

--- RegularExpression.py
+++ RegularExpression.py
@@ -15,6 +15,7 @@ if __name__ == '__main__':
     user = UserRegestration()
     name_pattern = "^[A-Z]{1}[a-z]{3,}$"
     email_pattern = "^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,3}([.][a-zA-Z]{2})*$"
+    phone_number_pattern = "^91 [0-9]{10}$"
 
     first_name = input("Enter the First Name : ")
     print(user.validation(name_pattern, first_name))
@@ -22,3 +23,5 @@ if __name__ == '__main__':
     print(user.validation(name_pattern, last_name))
     user_email = input("Enter your Email Address : ")
     print(user.validation(email_pattern, user_email))
+    phone_number = input("Enter your Phone number : ")
+    print(user.validation(phone_number_pattern, phone_number))

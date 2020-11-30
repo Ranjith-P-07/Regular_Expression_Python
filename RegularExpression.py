05068c1c8b7ad5e71504acc1de781564566363d6
“Ranjith” <Ranjithroy704@gmail.com>
2020-12-01 00:08:12 +0530

    Add : Email Validation Added

--- RegularExpression.py
+++ RegularExpression.py
@@ -14,7 +14,11 @@ class UserRegestration:
 if __name__ == '__main__':
     user = UserRegestration()
     name_pattern = "^[A-Z]{1}[a-z]{3,}$"
+    email_pattern = "^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,3}([.][a-zA-Z]{2})*$"
+
     first_name = input("Enter the First Name : ")
     print(user.validation(name_pattern, first_name))
     last_name = input("Enter your Last Name: ")
     print(user.validation(name_pattern, last_name))
+    user_email = input("Enter your Email Address : ")
+    print(user.validation(email_pattern, user_email))

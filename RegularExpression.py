73ecb3f4f6b852fe0ac82f04c0d0a875fdc7ac28
“Ranjith” <Ranjithroy704@gmail.com>
2020-11-30 23:08:49 +0530

    Add: Last Name Validation

--- RegularExpression.py
+++ RegularExpression.py
@@ -8,3 +8,11 @@ if re.search(pattern, user_input):
     print("Valid First Name ")
 else:
     print("Invalid First Name")
+
+pattern1 = "^[A-Z]{1}[a-z]{3,}$"
+user_input1 = input("Enter your Last Name : ")
+
+if re.search(pattern1, user_input1):
+    print("Valid Last Name")
+else:
+    print("Invalid Last Name")

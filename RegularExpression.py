858d5411fdc3ea680665c9237343add80fe761d4
“Ranjith” <Ranjithroy704@gmail.com>
2020-11-30 23:50:37 +0530

    Add: last Name Validation

--- RegularExpression.py
+++ RegularExpression.py
@@ -1,18 +1,20 @@
 # Created by Ranjith P at 30-11-2020
 import re
 
-pattern = "^[A-Z]{1}[a-z]{3,}$"
-user_input = input("Enter your First Name : ")
 
-if re.search(pattern, user_input):
-    print("Valid First Name ")
-else:
-    print("Invalid First Name")
+class UserRegestration:
 
-pattern1 = "^[A-Z]{1}[a-z]{3,}$"
-user_input1 = input("Enter your Last Name : ")
+    def validation(self, pattern, input):
+        if re.search(pattern, input):
+            return "Valid Input"
+        else:
+            return "Invalid Input"
 
-if re.search(pattern1, user_input1):
-    print("Valid Last Name")
-else:
-    print("Invalid Last Name")
+
+if __name__ == '__main__':
+    user = UserRegestration()
+    name_pattern = "^[A-Z]{1}[a-z]{3,}$"
+    first_name = input("Enter the First Name : ")
+    print(user.validation(name_pattern, first_name))
+    last_name = input("Enter your Last Name: ")
+    print(user.validation(name_pattern, last_name))

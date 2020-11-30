f0344a3825e63ecfe99fc32c26259fd345592ac7
“Ranjith” <Ranjithroy704@gmail.com>
2020-11-30 23:42:28 +0530

    First Name Validation

--- RegularExpression.py
+++ RegularExpression.py
@@ -1,10 +1,18 @@
 # Created by Ranjith P at 30-11-2020
 import re
 
-pattern = "^[A-Z]{1}[a-z]{3,}$"
-user_input = input("Enter your First Name : ")
 
-if re.search(pattern, user_input):
-    print("Valid First Name ")
-else:
-    print("Invalid First Name")
+class UserRegestration:
+
+    def validation(self, pattern, input):
+        if re.search(pattern, input):
+            return "Valid Input"
+        else:
+            return "Invalid Input"
+
+
+if __name__ == '__main__':
+    user = UserRegestration()
+    name_pattern = "^[A-Z]{1}[a-z]{3,}$"
+    first_name = input("Enter the First Name :")
+    print(user.validation(name_pattern, first_name))
